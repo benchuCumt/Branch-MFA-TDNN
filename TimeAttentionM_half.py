0bc5422bb7b845446e6d02eef9335f6658bc3f4d
benchuCumt <73736249+benchuCumt@users.noreply.github.com>
2025-07-13 11:43:25 +0800

    Update TimeAttentionM_half.py

--- TimeAttentionM_half.py
+++ TimeAttentionM_half.py
@@ -3,16 +3,12 @@ import torch
 
 
 def overlap_chunks(tensor, chunk_size=50, step=50):
-    # 计算块的重叠
     overlap = chunk_size - step
-    # 计算可以创建的块的数量
     if overlap == 0:
         num_chunks = int(tensor.size(-1) / chunk_size)
     else:
         num_chunks = int((tensor.size(-1) - chunk_size) / step + 1)
-    # 创建一个列表来保存重叠的块
     chunks = []
-    # 生成重叠的块
     for i in range(num_chunks):
         start_idx = i * step
         end_idx = start_idx + chunk_size
@@ -22,12 +18,11 @@ def overlap_chunks(tensor, chunk_size=50, step=50):
 
 
 class TimeAttention_half(nn.Module):
-    def __init__(self, ratio=6):  # 这里的dims要根据分段数确定
+    def __init__(self, ratio=6):  
         super(TimeAttention_half, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
         self.dims = 50
-        # 不同段使用不同的全连接层，来表示数据的不同分布
         self.fc1 = nn.Sequential(nn.Conv2d(self.dims, self.dims // ratio, 1, bias=False),
                                  nn.ReLU(),
                                  nn.Conv2d(self.dims // ratio, self.dims, 1, bias=False))
@@ -44,15 +39,12 @@ class TimeAttention_half(nn.Module):
 
     def forward(self, x):
         outs = []
-        # 用来控制全连接层序号的索引
         i = 1
-        # 对x进行分组，这里训练和验证都需要分组，因为一段改成0.5秒了
         xs = overlap_chunks(x)
         for x in xs:
             x = torch.transpose(x, 1, 3)
             ap_out = self.avg_pool(x)
-            # 这里过的全连接层的序号需要计算
-            # 这里过的全连接层的序号需要计算
+            # j = i mod m
             index = i % 4
             if index == 1:
                 avg_out = self.fc1(ap_out)
@@ -60,26 +52,11 @@ class TimeAttention_half(nn.Module):
                 avg_out = self.fc2(ap_out)
             else:
                 avg_out = self.fc3(ap_out)
-            # 仅使用平均池化
             out = avg_out
             out = torch.transpose(out, 1, 3)
             outs.append(out)
             i = i + 1
         att = torch.cat(outs, dim=3)
-        # 最后过sigmoid，因为有重叠
+        # sigmoid
         att = self.sigmoid(att)
         return att
-
-
-# # 加载模型，参数是时间维度的维数
-# model = TimeAttention_half()
-#
-# # 准备输入数据
-# input_data = torch.randn(20, 32, 20, 900)  # 输入数据的大小要与模型设定一致
-#
-# # 前向传播
-# output = model.forward(input_data)
-#
-# # 获取输出层大小
-# print("attention形状：", output.shape)
-
